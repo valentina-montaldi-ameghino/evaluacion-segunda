e7b643198e206e6775ff87e7902285ffbcdb500e
damianlattenero <damianlattenero@gmail.com>
2016-08-26 09:52:01 -0300

    first commit

---
+++ src/main/java/DummyClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by damian on 26/08/16.
+ */
+public class DummyClass {
+    public String  variable;
+
+    public DummyClass(String hello) {
+        this.variable = hello;
+    }
+}
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by damian on 26/08/16.
+ */
+public class DummyTest {
+    @Test
+    public void dummyTest(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void dummClass(){
+        DummyClass d = new DummyClass("hello");
+
+        Assert.assertEquals("hello", d.variable);
+    }
+}
