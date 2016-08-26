281b8fd15529da99dcec135b2b503f93157cd37f
damianlattenero <damianlattenero@gmail.com>
2016-08-26 15:01:44 -0300

    Test de figuras

---
+++ src/test/java/FigurasTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by damian on 26/08/16.
+ */
+public class FigurasTest{
+/*
+    @Test
+    public void calcularArea() {
+        Figura fig = new Rectangulo(5,10);
+
+        Assert.assertEquals(50, fig.area());
+
+    }
+
+    @Test
+    public void calcularPerimetro() {
+        Figura fig = new Circulo(10);
+
+        Assert.assertEquals(Math.PI*10, fig.perimetro());
+
+    }
+
+    @Test
+    public void calcularAreas(){
+        Figura fig  = Cuadrado(5);
+        Figura fig2 = Rectangulo(5,10);
+        Figura fig3 = Circulo(20);
+
+        List<Figura> figuras = Arrays.asList(fig,fig2,fig3);
+
+        Assert.assertEquals(475, figuras.sumarAreas(figuras));
+    }*/
+}
