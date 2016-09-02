480b148e7bc5d4a3e952bb10d2b1556aad49823a
damianlattenero <damianlattenero@gmail.com>
2016-09-02 13:34:53 -0300

    fix en tests

---
+++ src/main/java/Par.java
@@ -0,0 +1,19 @@
+/**
+ * Created by damian on 02/09/16.
+ */
+public class Par {
+
+    private int first;
+    private int second;
+
+    public Par(int i, int i1) {
+    }
+
+    public int first() {
+        return this.first;
+    }
+
+    public int second() {
+        return this.second;
+    }
+}
---
+++ src/test/java/ParesOrdenadosTest.java
@@ -0,0 +1,49 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.omg.CORBA.DoubleHolder;
+
+/**
+ * Created by damian on 02/09/16.
+ */
+public class ParesOrdenadosTest {
+    /*
+    @Test
+    public void crearParOrdenado(){
+        //Tipar la Clase Par
+        Par dosTres = new Par(2,3);
+
+        Assert.assertEquals(dosTres.first(), 2);
+        Assert.assertEquals(dosTres.second(), 3);
+    }
+
+    @Test
+    public void elPrimero(){
+        Par dosTres = new Par("casa",3);
+
+        Assert.assertEquals(dosTres.first(), "casa");
+    }
+
+    @Test
+    public void elSegundo(){
+        Cuadrado c = new Cuadrado(2);
+        Par dosTres = new Par("casa",c);
+
+        Assert.assertEquals(dosTres.second(), c);
+    }
+
+    @Test
+    public void elPrimero2(){
+        //Tipar las siguientes variables:
+
+        Par par = new Par("casa",2d);
+        Cuadrado c = new Cuadrado(1);
+        Rectangulo r = new Rectangulo(2,3);
+        Par par2 = new Par(c,r);
+
+        Par parGroso = new Par(par,par2);
+
+        //acceder a la base del rectangulo que esta dentro de la variable parGroso
+        Assert.assertEquals(parGroso , r.area(),0);
+
+    }*/
+}
